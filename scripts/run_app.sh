862cb794972e741cc7a918769da8a9ee60b60559
Alex <shevashokc@gmail.com>
2020-09-03 21:01:23 +0300

    added LB config

--- scripts/run_app.sh
+++ scripts/run_app.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 apt update
-apt install -fy python-setuptools build-essential python-dev
-easy_install flask psutil
+apt install -fy python-setuptools build-essential python3 python python-dev python3-flask easy_install psutil
 
 wget https://github.com/AlexViki/laba_13_gpc_infra_lb_vm/tree/master/app/src/app.py -O app.py
 FLASK_APP=app.py flask run --host=0.0.0.0 --port=80
