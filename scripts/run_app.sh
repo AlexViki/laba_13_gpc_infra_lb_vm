eb4fbbf0d7f1dfc408d989039204b5ca21908ca6
Alex <shevashokc@gmail.com>
2020-09-03 21:10:12 +0300

    add packeges

--- scripts/run_app.sh
+++ scripts/run_app.sh
@@ -2,7 +2,7 @@
 
 apt update
 
-apt install -fy python-setuptools build-essential python3 python python-dev python3-flask easy_install psutil
+apt install -fy python-setuptools build-essential python3 python python-dev python3-flask python-pip
 
 wget https://github.com/AlexViki/laba_13_gpc_infra_lb_vm/tree/master/app/src/app.py -O app.py
 FLASK_APP=app.py flask run --host=0.0.0.0 --port=80
